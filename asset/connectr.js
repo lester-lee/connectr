1463f634928a90dd9462a2406d9160f983beaec7
Lester Lee <lester.lee17@gmail.com>
2017-01-22 14:39:56 -0500

    added search bar

--- asset/connectr.js
+++ asset/connectr.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
         $('.nav-extension').removeClass('show-content');
         $(extendID).addClass('show-content');
     });
+
+    $('.searchbar').on('keyup', function() {
+        var filter, list, li, i;
+        filter = $(this)[0].value.toUpperCase();
+        list = $(this).siblings('ul');
+        li = list[0].getElementsByTagName('li');
+        for (i = 0; i < li.length; i++) {
+            if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    });
 });
